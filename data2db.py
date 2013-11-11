1f84a6ba3469ad00620cc34bfdfd18135a003c77
xsongx <xsongx.nudt@gmail.com>
2013-11-11 16:07:00 +0800

    20131111

--- data2db.py
+++ data2db.py
@@ -6,50 +6,156 @@ Created on Mon Oct 21 17:16:01 2013
 """
 
 from weibo_tang import weibodb
-import os
-from pattern.db import date, time
-def usermap(filename):
-    f=open(filename,'r')
+import codecs
+from pattern.db import date,DateError
+def usermap(filename,dbname):
+    f=codecs.open(filename,'rb',encoding='UTF-8')
+#    f = codecs.EncodedFile(f,file_encoding = "utf8",data_encoding ="gb2312")
     usr_id=[]
-    usr_id=[uid for uid in f]
-    return usr_id
-def user2dict(filename,mapid,db):
-    f=open(filename,'r')
-    filelines=f.readlines()
-    targetlines=filelines[13:]
-    print ''.join(targetlines[:100])
+    usr_id=[uid.strip() for uid in f]
     mapdict={}
-    for (i,uid) in enumerate(mapid):
-        mapdict[uid]=i
-    while(1):
+    for (i,uid) in enumerate(usr_id):
+        mapdict["userid"]=uid
+        mapdict["mapid"]=i
+        dbname.userInsert(mapdict)
+        print uid+':'+str(i)
+#    return mapdict
+def user2dict(filename):
+    f=codecs.open(filename,'rb',encoding='UTF-8')
+#    f = codecs.EncodedFile(f,file_encoding = "utf8",data_encoding ="gb2312")
+    filelines=f.readlines()
+    print ''.join(filelines[:14])
+    i=15
+    while(i<len(filelines)):
+#        print "line"+str(i)
         try:
-            userdict={}
-            sublines=targetlines[:14]
-            if sublines[0] in mapdict.keys():
-                userdict["mapid"]=mapdict[sublines[0]]
-            else:
-                userdict["mapid"]=0
-            userdict["biFollowersCount"]=int(sublines[1])
-            userdict["city"]=sublines[2]
-            userdict["verified"]=sublines[3]
-            userdict["followersCount"]=int(sublines[4])
-            userdict["location"]=sublines[5]
-            userdict["province"]=sublines[6]
-            userdict["friendsCount"]=int(sublines[7])
-            userdict["name"]=sublines[8]
-            userdict["gender"]=sublines[9]
-            userdict["createdAt"]=date(sublines[10])
-            userdict["verifiedType"]=sublines[11]
-            userdict["statusesCount"]=int(sublines[12])
-            userdict["description"]=sublines[13]
-            db.userInsert(userdict)
-            targetlines=targetlines[14:]
+            
+            try:
+                sublines=filelines[i:i+15]
+            except:
+                sublines=filelines[i:]
+#            print ''.join(sublines)
+#            if sublines[0].strip() in mapid.keys():
+#                userdict["mapid"]=mapid[sublines[0].strip()]
+#            else:
+#                userdict["mapid"]=-1
+#            print sublines[0].strip()+' mapto: '+str(userdict["mapid"])
+            userid=sublines[0].strip()
+            try:
+                biFollowersCount=int(sublines[1].strip())
+            except:
+                biFollowersCount=0
+            city=sublines[2].strip()
+            verified=sublines[3].strip()
+            try:
+                followersCount=int(sublines[4].strip())
+            except:
+                followersCount=0
+            location=sublines[5].strip()
+            province=sublines[6].strip()
+            friendsCount=int(sublines[7].strip())
+            name=sublines[8].strip().replace("\\","\\\\")
+            gender=sublines[9].strip()
+            timelist=sublines[10].strip().split('-')
+            timestr='-'.join(timelist[:-1])+' '+timelist[-1]
+            createdAt=date(timestr)
+            verifiedType=sublines[11].strip()
+            try:
+                statusesCount=int(sublines[12].strip())
+            except:
+                statusesCount=0
+            description=sublines[13].strip().replace("\\","\\\\")
+            i=i+15
+            userrec=[]
+            userrec.append(userid)
+            userrec.append(name)
+            userrec.append(province)
+            userrec.append(city)
+            userrec.append(location)
+            userrec.append(description)
+            userrec.append(gender)
+            userrec.append(followersCount)
+            userrec.append(friendsCount)
+            userrec.append(statusesCount)
+            userrec.append(createdAt)
+            userrec.append(verified)
+            userrec.append(verifiedType)
+            userrec.append(biFollowersCount)
+            yield userrec
         except:
+            print filename +" error!"
+            exit(0)
+#        except:
+#            pass
+
+def user2db(userprofile,dbname):
+    a=True
+    user_dict=user2dict(userprofile)
+    while a:
+        try:
+            user=user_dict.next()
+            dbname.userUpdate(user)
+        except StopIteration:
+            a=False
+            print userprofile+" Has been processed!"
+#                fileprocessed.write(f+'\n')
             pass
-                
+#        except :
+#            print "error!!!"
+#            pass
+            
+            
+def staticRel(filename,db):
+    f=open(filename,'r')
+#    flines=f.readlines(30)
+#    print ''.join(flines)
+    line1=f.readline().strip()
+    num=line1.split()
+    i=0
+    j=0
+    while(i<int(num[0])):
+        rel_line=f.readline().split()
+        reldict={}
+        userv1=int(rel_line[0])
+        reldict['user_v1']=userv1
+        followees=int(rel_line[1])
+        rel_line=rel_line[2:]
+        userrel=[]
+#        for rel in range(followees):
+#            reldict['user_v2']=int(rel_line[2*rel])
+#            reldict['re_0']=int(rel_line[2*rel+1])
+#            db.relationInsert(reldict)
+        for rel in range(followees): 
+            j=j+1
+            user_v2=int(rel_line[2*rel])
+            re_0=int(rel_line[2*rel+1])
+            userrel.append((j,userv1,user_v2,re_0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1))
+        db.relBatchInsert(userrel)
+        i=i+1
+        print str(j)+":"+str(userv1)+" static!"
+
+def dynamicRel(filename,db):
+    f=open(filename,'r')
+    t=True
+    while t:
+        try:
+            fline=f.readline().strip()
+            rel_line=fline.split()
+            user_v1=int(rel_line[0])
+            user_v2=int(rel_line[1])
+            rel="re_"+rel_line[-1]
+            reldict=(user_v1,user_v2,rel)
+            db.relationInsert(reldict)
+        except:
+            t=False
+            print "over!"
+            exit(0)
 
 mydb=weibodb("tangdb")
-user_map=usermap("/media/work_/tangjie/user_list.txt")
-user2dict("/media/work_/tangjie/user_profile1.txt",user_map,mydb)
-user2dict("/media/work_/tangjie/user_profile2.txt",user_map,mydb)              
+#user_map=usermap("/media/M_fM__VM_0M_eM__JM__M_eM__MM_7/tangjie/uidlist.txt",mydb)
+#user2db("/media/M_fM__VM_0M_eM__JM__M_eM__MM_7/tangjie/user_profile1-1.txt",mydb)
+#user2db("/media/M_fM__VM_0M_eM__JM__M_eM__MM_7/tangjie/user_profile2-1.txt",mydb)
+#staticRel("/media/M_fM__VM_0M_eM__JM__M_eM__MM_7/tangjie/weibo_network.txt",mydb)
+dynamicRel("/media/M_fM__VM_0M_eM__JM__M_eM__MM_7/tangjie/graph_170w_1month.txt",mydb)
+             
     
--- weibo_tang.py
+++ weibo_tang.py
@@ -10,18 +10,19 @@ from pattern.db import Database,MYSQL
 from pattern.db import field, pk, STRING,UNIQUE,INTEGER,DATE,TEXT
 from pattern.db import filter,all
 from MySQLdb import IntegrityError
+import MySQLdb
 class weibodb(Database):
     def __init__(self, database):
         self.db=Database(database,password = '20090924',type=MYSQL)
         self.userTable()
 #        self.statusTable()
-#        self.networkTable()
+        self.relationTable()
     def userTable(self):
         if not "usertable" in self.db:
             self.db.create("usertable",fields=(
              pk(), # Auto-incremental id.
-             field("mapid",INTEGER,index=UNIQUE),#用户映射id
-             field("userid",STRING(20)),#用户UID
+             field("mapid",INTEGER),#用户映射id
+             field("userid",STRING(20),index=UNIQUE),#用户UID
              field("name",STRING(50)),#友好显示名称
              field("province",STRING(20)),#用户所在省级ID
              field("city",STRING(20)),#	int	用户所在城市ID
@@ -36,40 +37,137 @@ class weibodb(Database):
              field("verifiedType",STRING(20)),#	int	暂未支持
              field("biFollowersCount",INTEGER)#	int	用户的互粉数
             ))
+    def relationTable(self):
+        if not "relationtable" in self.db:
+            self.db.create("relationtable",fields=(
+             pk(), # Auto-incremental id.
+             field("user_v1",INTEGER),#用户1UID
+             field("user_v2",INTEGER),#用户2UID
+             field("re_0",INTEGER,default=-1),#初始关系
+             field("re_1",INTEGER,default=-1),#关系变化
+             field("re_2",INTEGER,default=-1),#关系变化
+             field("re_3",INTEGER,default=-1),#关系变化
+             field("re_4",INTEGER,default=-1),#关系变化
+             field("re_5",INTEGER,default=-1),#关系变化
+             field("re_6",INTEGER,default=-1),#关系变化
+             field("re_7",INTEGER,default=-1),#关系变化
+             field("re_8",INTEGER,default=-1),#关系变化
+             field("re_9",INTEGER,default=-1),#关系变化
+             field("re_10",INTEGER,default=-1),#关系变化
+             field("re_11",INTEGER,default=-1),#关系变化
+             field("re_12",INTEGER,default=-1),#关系变化
+             field("re_13",INTEGER,default=-1),#关系变化
+             field("re_14",INTEGER,default=-1),#关系变化
+             field("re_15",INTEGER,default=-1),#关系变化
+             field("re_16",INTEGER,default=-1),#关系变化
+             field("re_17",INTEGER,default=-1),#关系变化
+             field("re_18",INTEGER,default=-1),#关系变化
+             field("re_19",INTEGER,default=-1),#关系变化
+             field("re_20",INTEGER,default=-1),#关系变化
+             field("re_21",INTEGER,default=-1),#关系变化
+             field("re_22",INTEGER,default=-1),#关系变化
+             field("re_23",INTEGER,default=-1),#关系变化
+             field("re_24",INTEGER,default=-1),#关系变化
+             field("re_25",INTEGER,default=-1),#关系变化
+             field("re_26",INTEGER,default=-1),#关系变化
+             field("re_27",INTEGER,default=-1),#关系变化
+             field("re_28",INTEGER,default=-1),#关系变化
+             field("re_29",INTEGER,default=-1),#关系变化
+             field("re_30",INTEGER,default=-1),#关系变化
+             field("re_31",INTEGER,default=-1),#关系变化
+             field("re_32",INTEGER,default=-1)#关系变化
+            ))
+            
     def userInsert(self,userdict):
-        for f in self.db.usertable.fields:
-            if f not in userdict.keys():
-                userdict[f]=u''
         try:
             self.db.usertable.append(
              mapid=userdict["mapid"],#用户映射id
-             userid=userdict["id"],#用户UID
-             name=userdict["name"],#友好显示名称
-             province=userdict["province"],#用户所在省级ID
-             city=userdict["city"],#	int	用户所在城市ID
-             location=userdict["location"],#	string	用户所在地
-             description=userdict["description"],#	string	用户个人描述
-             gender=userdict["gender"],#	string	性别，m：男、f：女、n：未知
-             followersCount=userdict["followersCount"],#	int	粉丝数
-             friendsCount=userdict["friendsCount"],#	int	关注数
-             statusesCount=userdict["statusesCount"],#	int	微博数
-             createdAt=userdict["createdAt"],#	string	用户创建（注册）时间
-             verified=userdict["verified"],#	STRING(10)	是否是微博认证用户，即加V用户，true：是，false：否
-             verifiedType=userdict["verifiedType"],#	int	暂未支持
-             biFollowersCount=userdict["biFollowersCount"]#	int	用户的互粉数
+             userid=userdict["userid"]#用户UID
              )
         except IntegrityError:
-            print "User"+str(userdict['id'])+" has existed!"
-            q=self.db.usertable.search(fields=['mapid','province','city','location','description','gender','verified','verifiedType'],
-                                       filters=all(filter("userid",userdict['id'])))
+            print "User"+str(userdict['userid'])+" has existed!"
+        except:
+            print "error when inserting user recorder:"+str(userdict['userid'])+"!!!!"
+            for kk,vv in userdict.items():
+                print str(kk)+'='+str(vv)
+            pass   
+    def userUpdate(self,userdict):
+        try:
+            q=self.db.usertable.search(fields=['id','mapid','userid'],
+                                       filters=all(filter("userid",userdict[0])))#
             for row in q.rows():
                 a=row
-            for (i,fieldid) in enumerate(q.fields):
-                if (a[i]==u'null' or a[i]==u''):
-                    if (userdict[fieldid]!=u'null' and userdict[fieldid]!=u''):
-                        self.db.usertable.update(a[0],{fieldid:userdict[fieldid]})
-                        print "User"+str(userdict['id'])+fieldid+" has updated!"
+            if (a):
+                userdict.insert(0,a[1])
+                userup=tuple(userdict)
+#                self.db.usertable.update(a[0],userup)
+                self.db.usertable.update(a[0],name=userdict[2],province=userdict[3],city=userdict[4],
+                                         location=userdict[5],description=userdict[6],gender=userdict[7],
+                                            followersCount=userdict[8],friendsCount=userdict[9],
+                                            statusesCount=userdict[10],createdAt=userdict[11],
+                                            verified=userdict[12],verifiedType=userdict[13],biFollowersCount=userdict[14])
+                print "User "+str(userdict[1])+" has updated!"            
+            else:
+                userdict.insert(0,-1)
+                userup=tuple(userdict)
+                self.db.usertable.append(userup)
+#                self.db.usertable.append(
+#    #             mapid=userdict["mapid"],#用户映射id
+#                 userid=userdict["userid"],#用户UID
+#                 name=userdict["name"],#友好显示名称
+#                 province=userdict["province"],#用户所在省级ID
+#                 city=userdict["city"],#	int	用户所在城市ID
+#                 location=userdict["location"],#	string	用户所在地
+#                 description=userdict["description"],#	string	用户个人描述
+#                 gender=userdict["gender"],#	string	性别，m：男、f：女、n：未知
+#                 followersCount=userdict["followersCount"],#	int	粉丝数
+#                 friendsCount=userdict["friendsCount"],#	int	关注数
+#                 statusesCount=userdict["statusesCount"],#	int	微博数
+#                 createdAt=userdict["createdAt"],#	string	用户创建（注册）时间
+#                 verified=userdict["verified"],#	STRING(10)	是否是微博认证用户，即加V用户，true：是，false：否
+#                 verifiedType=userdict["verifiedType"],#	int	暂未支持
+#                 biFollowersCount=userdict["biFollowersCount"]#	int	用户的互粉数
+#                 )
+                print "user "+"User "+str(userdict[1])+" has appended!"
+        except IntegrityError:
+            print "User"+str(userdict['userid'])+" has existed!"         
             pass
-        except TypeError:
-            print "error when inserting user recorder:"+str(userdict['id'])+"!!!!"
+        except:
+            print "error when updating user recorder:"+str(userdict['userid'])+"!!!!"
+            for kk,vv in userdict.items():
+                print str(kk)+'='+str(vv)
+            exit(0)
+    def relBatchInsert(self,rellist):
+        conn = MySQLdb.connect(host='localhost',user='root',passwd='20090924',charset='utf8')
+        cursor = conn.cursor()
+        try:
+            DB_NAME = 'tangdb'
+            conn.select_db(DB_NAME)
+            cursor.executemany('INSERT INTO relationtable values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',rellist)
+#            for rel in rellist:           
+#                self.db.relationtable.append((rel))
+            conn.commit()
+        except:
+            print "batch insert error!!"
+            exit(0)
+        cursor.close()
+        conn.close() 
+    def relationInsert(self,reldict):
+        try:
+            q=self.db.relationtable.search(fields=['id'],filters=all(filter("user_v1",reldict[0]),filter("user_v2",reldict[1])))
+            if (q.rows()):
+                for row in q.rows():
+                    a=row
+                if (a):
+                    self.db.relationtable.update(a[0],{reldict[-1]:0})
+                    print "Relation between User "+str(reldict[0])+" and User "+str(reldict[1])+" has updated!"
+                else:
+                    self.db.relationtable.append({'user_v1':reldict[0],'user_v2':reldict[1],reldict[-1]:0})
+                    print "Relation between User "+str(reldict[0])+" and User "+str(reldict[1])+" has inserted!"
+            else:
+                self.db.relationtable.append({'user_v1':reldict[0],'user_v2':reldict[1],reldict[-1]:0})
+                print "Relation between User "+str(reldict[0])+" and User "+str(reldict[1])+" has inserted!"
+        except:
+            print "error when inserting relation recorder:"+str(reldict[0])+' and '+str(reldict[1])+"!!!!"
+            pass
             exit(0)
